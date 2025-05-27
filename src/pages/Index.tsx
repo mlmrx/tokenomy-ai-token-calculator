db7704457d9140f06ad5a4993a5842e551845701
Mahesh <116022563+mlmrx@users.noreply.github.com>
2025-05-27 06:35:56 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -86,7 +86,7 @@ const Index = () => {
           {/* Content Grid */}
           <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
             {/* Main Content */}
-            <div className="lg:col-span-3">
+            <div className="lg:col-span-4">
               {renderActiveComponent()}
             </div>
 
